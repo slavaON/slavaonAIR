3494d43b9ef108d999f11b37920ae76c78209f42
Sviat <97764293+slavaON@users.noreply.github.com>
2022-03-15 21:26:46 +0200

    Create just.js

---
+++ just.js
@@ -0,0 +1,7 @@
+class Block {
+    constructor(timestamp = "", data = []) {
+        this.timestamp = timestamp;
+        // в this.data должна содержаться информация наподобие сведений о транзакциях.
+        this.data = data;
+    }
+}
